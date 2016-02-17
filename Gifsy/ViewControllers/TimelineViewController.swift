25b179b36afc0111a823097f940f1e53ae9191ea
Martin Spier <spiermar@gmail.com>
2016-02-17 13:51:35 -0800

    Fetching posts from Parse.

--- Gifsy/ViewControllers/TimelineViewController.swift
+++ Gifsy/ViewControllers/TimelineViewController.swift
@@ -9,21 +9,16 @@
 import UIKit
 import Parse
 
-var photoTakingHelper: PhotoTakingHelper?
+
 
 class TimelineViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.tabBarController?.delegate = self
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBOutlet weak var tableView: UITableView!
     
+    var posts: [Post] = []
+    
+    var photoTakingHelper: PhotoTakingHelper?
+
     func takePhoto() {
         // instantiate photo taking class, provide callback for when photo  is selected
         photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!, callback: { (image: UIImage?) in
@@ -32,6 +27,55 @@ class TimelineViewController: UIViewController {
             post.uploadPost()
         })
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let followingQuery = PFQuery(className: "Follow")
+        followingQuery.whereKey("fromUser", equalTo:PFUser.currentUser()!)
+        
+        let postsFromFollowedUsers = Post.query()
+        postsFromFollowedUsers!.whereKey("user", matchesKey: "toUser", inQuery: followingQuery)
+        
+        let postsFromThisUser = Post.query()
+        postsFromThisUser!.whereKey("user", equalTo: PFUser.currentUser()!)
+        
+        let query = PFQuery.orQueryWithSubqueries([postsFromFollowedUsers!, postsFromThisUser!])
+        query.includeKey("user")
+        query.orderByDescending("createdAt")
+        
+        query.findObjectsInBackgroundWithBlock {(result: [PFObject]?, error: NSError?) -> Void in
+            self.posts = result as? [Post] ?? []
+            self.tableView.reloadData()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell")!
+        
+        cell.textLabel!.text = "Post"
+        
+        return cell
+    }
+    
 }
 
 // MARK: Tab Bar Delegate
