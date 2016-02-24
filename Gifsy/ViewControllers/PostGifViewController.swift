50f7e09372cdc123bc1a913c647a74975cbbe320
Martin Spier <spiermar@gmail.com>
2016-02-23 17:22:31 -0800

    New post gif view.

---
+++ Gifsy/ViewControllers/PostGifViewController.swift
@@ -0,0 +1,15 @@
+//
+//  PostGifViewController.swift
+//  Gifsy
+//
+//  Created by Martin Spier on 2/23/16.
+//  Copyright © 2016 Gifsy. All rights reserved.
+//
+
+import UIKit
+
+class PostGifViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
